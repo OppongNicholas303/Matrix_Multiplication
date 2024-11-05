2efd28106da4110df818bb988e043917d645b153
nicholas <nicholas.oppong@amalitech.com>
2024-11-05 10:34:17 +0000

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int [][] matrixA = {{12, 2, 4}, {10, 1, 1}};
+        int [][] matrixB = {{2,2},{1,2}, {1,1}};
+
+        int [][] matrixC = new int[3][];
+
+        for (int i=0; i<matrixB.length; i++){
+
+        }
+
+
+    }
+}
